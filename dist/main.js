874d2946616f86c71c721f3d7b4af3bc85fcea72
xxIrynaxx <irakalachenko2006@gmail.com>
2024-08-01 23:32:25 +0300

    Webpack - cli

--- dist/main.js
+++ dist/main.js
@@ -10,13 +10,13 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/my-index.js":
-/*!*************************!*\
-  !*** ./src/my-index.js ***!
-  \*************************/
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _profile_gateway_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./profile/gateway.js */ \"./src/profile/gateway.js\");\n/* harmony import */ var _profile_index_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./profile/index.js */ \"./src/profile/index.js\");\n\r\n\r\n\r\n(0,_profile_gateway_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])('facebook').then(userData =>\r\n  (0,_profile_index_js__WEBPACK_IMPORTED_MODULE_1__.printProfile)({\r\n    name: userData.name,\r\n    company: userData.location,\r\n  })\r\n);\r\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvbXktaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7O0FBQTZDO0FBQ0s7QUFDbEQ7QUFDQSwrREFBUztBQUNULEVBQUUsK0RBQVk7QUFDZDtBQUNBO0FBQ0EsR0FBRztBQUNIIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdGFzazEvLi9zcmMvbXktaW5kZXguanM/OTE1OCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgZmV0Y2hVc2VyIGZyb20gJy4vcHJvZmlsZS9nYXRld2F5LmpzJztcclxuaW1wb3J0IHsgcHJpbnRQcm9maWxlIH0gZnJvbSAnLi9wcm9maWxlL2luZGV4LmpzJztcclxuXHJcbmZldGNoVXNlcignZmFjZWJvb2snKS50aGVuKHVzZXJEYXRhID0+XHJcbiAgcHJpbnRQcm9maWxlKHtcclxuICAgIG5hbWU6IHVzZXJEYXRhLm5hbWUsXHJcbiAgICBjb21wYW55OiB1c2VyRGF0YS5sb2NhdGlvbixcclxuICB9KVxyXG4pO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/my-index.js\n");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _profile_gateway_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./profile/gateway.js */ \"./src/profile/gateway.js\");\n/* harmony import */ var _profile_index_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./profile/index.js */ \"./src/profile/index.js\");\n\r\n\r\n\r\n(0,_profile_gateway_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])('facebook').then(userData =>\r\n  (0,_profile_index_js__WEBPACK_IMPORTED_MODULE_1__.printProfile)({\r\n    name: userData.name,\r\n    company: userData.location,\r\n  })\r\n);\r\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7O0FBQTZDO0FBQ0s7QUFDbEQ7QUFDQSwrREFBUztBQUNULEVBQUUsK0RBQVk7QUFDZDtBQUNBO0FBQ0EsR0FBRztBQUNIIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdGFzazEvLi9zcmMvaW5kZXguanM/YjYzNSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgZmV0Y2hVc2VyIGZyb20gJy4vcHJvZmlsZS9nYXRld2F5LmpzJztcclxuaW1wb3J0IHsgcHJpbnRQcm9maWxlIH0gZnJvbSAnLi9wcm9maWxlL2luZGV4LmpzJztcclxuXHJcbmZldGNoVXNlcignZmFjZWJvb2snKS50aGVuKHVzZXJEYXRhID0+XHJcbiAgcHJpbnRQcm9maWxlKHtcclxuICAgIG5hbWU6IHVzZXJEYXRhLm5hbWUsXHJcbiAgICBjb21wYW55OiB1c2VyRGF0YS5sb2NhdGlvbixcclxuICB9KVxyXG4pO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.js\n");
 
 /***/ }),
 
@@ -100,7 +100,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/my-index.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
