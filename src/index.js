9485c9e76a0e40fcd79c707317b0eca622328273
xxIrynaxx <irakalachenko2006@gmail.com>
2024-08-01 23:33:57 +0300

    Webpack - cli

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fetchUser from './profile/gateway.js';
 import { printProfile } from './profile/index.js';
 
-fetchUser('facebook').then(userData =>
+fetchUser('github').then(userData =>
   printProfile({
     name: userData.name,
     company: userData.location,
